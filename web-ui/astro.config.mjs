173ef201d9c46af4d8b621f30ad81aa246a9e6e8
Simon Wiles <simonjwiles@gmail.com>
2023-05-01 15:19:06 -0700

    Store pnpm stores, caches, and runtime data in the docker image
    This means `pnpm` is not required on the host at all (same as with all the python deps), which is pleasing.  It does mean the images must be rebuilt when `package.json` or `pnpm-lock.yaml` are updated, but this isn't new.  It's a tiny bit hacky (but the cleanest I could come up with after much experimentation) so wants keeping half an eye on, but so far looks good, and the advantages are manifold.

--- web-ui/astro.config.mjs
+++ web-ui/astro.config.mjs
@@ -1,12 +1,25 @@
-import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
+import { searchForWorkspaceRoot } from "vite";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
+const config = defineConfig({
   trailingSlash: "always",
   integrations: [svelte(), tailwind()],
   vite: {
     cacheDir: process.env.VITE_CACHE_DIR || "node_modules/.vite",
   },
 });
+
+if (process.env.MODULES_DIR) {
+  // If MODULES_DIR is defined, we need to allow access for the vite dev server
+  //  (this is expected in the dockerized stack).
+  config.vite.server = {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), process.env.MODULES_DIR],
+    },
+  };
+}
+
+export default config;
