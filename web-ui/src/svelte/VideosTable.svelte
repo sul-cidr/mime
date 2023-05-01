7ea5f7381fbc8334ca70c5014664ac2f9c3daf4d
Simon Wiles <simonjwiles@gmail.com>
2023-05-01 16:12:33 -0700

    Alert on API server not available

--- web-ui/src/svelte/VideosTable.svelte
+++ web-ui/src/svelte/VideosTable.svelte
@@ -7,25 +7,29 @@
 
   import { API_BASE } from "@config";
 
-  let videoTableSource: TableSource;
+  import Icon from "@svelte/Icon.svelte";
+
+  let videoTableSource: TableSource | void;
 
   async function getVideos() {
     const response = await fetch(`${API_BASE}/videos/`);
     return await response.json();
   }
 
-  const updateVideoData = (): Promise<TableSource> =>
-    getVideos().then((data) => ({
-      head: ["Name", "Meta", "Frame Count"],
-      body: data.videos.map((video: VideoRecord) => [
-        video.video_name,
-        `${video.width}x${video.height}@${video.fps}fps`,
-        video.frame_count,
-      ]),
-      // Passed to the `on:selected` handler:
-      meta: data.videos.map((video: VideoRecord) => video),
-      // foot: [...],
-    }));
+  const updateVideoData = (): Promise<TableSource | void> =>
+    getVideos()
+      .then((data) => ({
+        head: ["Name", "Meta", "Frame Count"],
+        body: data.videos.map((video: VideoRecord) => [
+          video.video_name,
+          `${video.width}x${video.height}@${video.fps}fps`,
+          video.frame_count,
+        ]),
+        // Passed to the `on:selected` handler:
+        meta: data.videos.map((video: VideoRecord) => video),
+        // foot: [...],
+      }))
+      .catch((error) => error);
 
   const selectVideoHandler = ({ detail: video }: { detail: VideoRecord }) => {
     $currentVideo = video;
@@ -36,12 +40,23 @@
   });
 </script>
 
-{#if videoTableSource}
+{#if videoTableSource !== undefined && !(videoTableSource instanceof Error)}
   <Table
     source={videoTableSource}
     interactive={true}
     on:selected={selectVideoHandler}
   />
+{:else if videoTableSource instanceof Error}
+  <div class="alert variant-filled-error flex items-baseline">
+    <Icon
+      name="alert-triangle"
+      height="24px"
+      width="24px"
+      class="self-center mr-4"
+    />
+    Error fetching data from API server:
+    <span class="font-mono">{videoTableSource.message}</span>
+  </div>
 {:else}
   <p>Initializing database and fetching videos...</p>
   <ProgressBar />
