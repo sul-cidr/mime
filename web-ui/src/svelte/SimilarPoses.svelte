c1efe936b1f6f01d868cb6e8f1f2ec6e93deb0d2
Peter Broadwell <broadwell@stanford.edu>
2023-06-15 02:13:39 -0700

    Ability to display source image behind similarity pose excerpts

--- web-ui/src/svelte/SimilarPoses.svelte
+++ web-ui/src/svelte/SimilarPoses.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-  import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
-  import { LayerCake, Canvas } from "layercake";
+  import { RadioGroup, RadioItem, SlideToggle } from "@skeletonlabs/skeleton";
+  import { LayerCake, Canvas, Html } from "layercake";
   import Pose from "@svelte/Pose.svelte";
   import { formatSeconds } from "../lib/utils";
 
   import { API_BASE } from "@config";
   import { currentPose, currentVideo } from "@svelte/stores";
 
+  let showFrame: boolean = false;
   export let similarityMetric = "cosine";
   let poses: Array<PoseRecord>;
 
@@ -24,6 +25,64 @@
     return await response.json();
   }
 
+  const shiftToOrigin = (keypoints: CocoSkeletonWithConfidence, bbox:FixedLengthArray<number, 4>) => {
+    let newKeypoints: CocoSkeletonWithConfidence = [...keypoints];
+
+    for (let x:number = 0; x<keypoints.length; x+=3) {
+      newKeypoints[x] -= bbox[0];
+    }
+    for (let y:number = 1; y<keypoints.length; y+=3) {
+      newKeypoints[y] -= bbox[1];
+    }
+    return newKeypoints;
+  }
+
+  const getNormDims = (keypoints: CocoSkeletonNoConfidence) => {
+    let x_values:Array<number> = [];
+    let y_values:Array<number> = [];
+    for (let i:number = 0; i<keypoints.length; i++) {
+      if (keypoints[i] >= 0) {
+        if (i % 2 == 0) {
+          x_values.push(keypoints[i]);
+        } else {
+          y_values.push(keypoints[i]);
+        }
+      }
+    }
+    let min_x = Math.min(...x_values);
+    let max_x = Math.max(...x_values);
+    let min_y = Math.min(...y_values);
+    let max_y = Math.max(...y_values);
+
+    let width = max_x - min_x;
+    let height = max_y - min_y;
+
+    return [width, height];
+  }
+
+  const getExtent = (keypoints: CocoSkeletonWithConfidence) => {
+    let x_values:Array<number> = [];
+    let y_values:Array<number> = [];
+    for (let i:number = 0; i<keypoints.length; i++) {
+      if ((i % 3 == 0) && (keypoints[i+2] > 0)) {
+        x_values.push(keypoints[i]);        
+      } else if (((i-1) % 3 == 0) && (keypoints[i+1] > 0)) {
+        y_values.push(keypoints[i]);      
+      }
+    }
+
+    let min_x = Math.min(...x_values);
+    let max_x = Math.max(...x_values);
+    let min_y = Math.min(...y_values);
+    let max_y = Math.max(...y_values);
+
+    let width = max_x - min_x;
+    let height = max_y - min_y;
+
+    return [min_x, min_y, width, height];
+  }
+
+
   $: getPoseData(
     $currentPose.video_id,
     $currentPose.frame,
@@ -35,6 +94,7 @@
 <section
   class="variant-ghost-secondary px-4 pt-4 pb-8 flex flex-col gap-4 items-center"
 >
+  <div class="p-1 inline-flex items-center space-x-1 rounded-token">
   <RadioGroup>
     <RadioItem
       bind:group={similarityMetric}
@@ -52,6 +112,10 @@
       value="innerproduct">Inner Product</RadioItem
     >
   </RadioGroup>
+  <SlideToggle name="slider-label" bind:checked={showFrame} size="sm">
+    Show Image
+  </SlideToggle>
+</div>
   {#if poses}
     <div class="flex gap-4">
       <div class="card variant-ghost-tertiary drop-shadow-lg">
@@ -60,14 +124,14 @@
         </header>
         <div class="w-full aspect-[5/6] frame-display py-[20px] px-[10px]">
           <LayerCake>
-            <!-- {#if showFrame}
-        <Html zIndex={0}>
-          <img
-            src={`${API_BASE}/frame/${videoId}/${$currentFrame}/`}
-            alt={`Frame ${$currentFrame}`}
-          />
-        </Html>
-      {/if} -->
+          {#if showFrame}
+            <Html zIndex={0}>
+              <img class="pose-bg-display"
+              src={`${API_BASE}/frame/resize/${$currentPose.video_id}/${$currentPose.frame}/${getExtent($currentPose.keypoints).join(",")}|${getNormDims($currentPose.norm).join(",")}`}
+              alt={`Frame ${$currentPose.frame}, Pose: ${$currentPose.pose_idx + 1}`}
+              />
+            </Html>
+          {/if}
             <Canvas zIndex={1}>
               <Pose poseData={$currentPose.norm} normalizedPose={true} />
             </Canvas>
@@ -91,14 +155,14 @@
           </header>
           <div class="w-full aspect-[5/6] frame-display py-[20px] px-[10px]">
             <LayerCake>
-              <!-- {#if showFrame}
-        <Html zIndex={0}>
-          <img
-            src={`${API_BASE}/frame/${videoId}/${$currentFrame}/`}
-            alt={`Frame ${$currentFrame}`}
-          />
-        </Html>
-      {/if} -->
+              {#if showFrame}
+                <Html zIndex={0}>
+                  <img class="pose-bg-display"
+                  src={`${API_BASE}/frame/resize/${pose.video_id}/${pose.frame}/${getExtent(pose.keypoints).join(",")}|${getNormDims(pose.norm).join(",")}`}
+                  alt={`Frame ${pose.frame}, Pose: ${pose.pose_idx + 1}`}
+                  />
+                </Html>
+              {/if}
               <Canvas zIndex={1}>
                 <Pose poseData={pose.norm} normalizedPose={true} />
               </Canvas>
@@ -120,5 +184,10 @@
   .frame-display {
     background: radial-gradient(circle at 50% -250%, #333, #111827, #333);
     box-shadow: inset 0px 0px 30px 0px #666;
+  }
+  .pose-bg-display {
+    height: 100%;
+    display: block;
+    margin: auto
   }
 </style>
