e9498f562fdff76baad200df67d6f36538c1d7bb
Simon Wiles <simonjwiles@gmail.com>
2023-06-21 10:56:53 -0700

    Python linting

--- api/mime_db/_read_only.py
+++ api/mime_db/_read_only.py
@@ -58,7 +58,7 @@ async def get_pose_data_by_frame(self, video_id: UUID) -> list:
 
 async def get_pose_data_from_video(self, video_id: UUID) -> list:
     return await self._pool.fetch(
-        f"SELECT * FROM pose WHERE video_id = $1 ORDER BY frame ASC;",
+        "SELECT * FROM pose WHERE video_id = $1 ORDER BY frame ASC;",
         video_id,
     )
 
@@ -79,7 +79,7 @@ async def get_unique_track_ids_from_video(self, video_id: UUID) -> list:
 
 async def get_video_id(self, video_name: str) -> asyncpg.Record:
     return await self._pool.fetchval(
-        f"SELECT id FROM video WHERE video_name = $1;", video_name
+        "SELECT id FROM video WHERE video_name = $1;", video_name
     )
 
 
