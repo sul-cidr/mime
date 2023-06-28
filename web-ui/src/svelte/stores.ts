933bec0e904d7a6b05f96ba8807de52a43d00c35
Peter Broadwell <broadwell@stanford.edu>
2023-06-28 01:21:27 -0700

    Dedicated store for the frames containing similar poses

--- web-ui/src/svelte/stores.ts
+++ web-ui/src/svelte/stores.ts
@@ -3,3 +3,4 @@ import { Writable, writable } from "svelte/store";
 export const currentVideo: Writable<VideoRecord> = writable();
 export const currentFrame: Writable<number | undefined> = writable();
 export const currentPose: Writable<PoseRecord> = writable();
+export const similarPoseFrames: Writable<{[frameno: number]: number}> = writable({});
