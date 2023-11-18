36e9fb39ac917d4b19453163c43881a75742b782
Simon Wiles <simonjwiles@gmail.com>
2023-11-17 20:01:12 -0800

    WIP

--- web-ui/src/svelte/UrlParamsProvider.svelte
+++ web-ui/src/svelte/UrlParamsProvider.svelte
@@ -2,23 +2,30 @@
   import { currentPose, currentVideo } from "@svelte/stores";
   import { API_BASE } from "@config";
 
-  const loadDataFromParams = async () => {
-    const urlParams = Object.fromEntries(
-      new URLSearchParams(window.location.search),
-    );
+  const urlParams = Object.fromEntries(
+    new URLSearchParams(window.location.search),
+  );
 
+  const loadDataFromParams = async () => {
     const { videos } = await (await fetch(`${API_BASE}/videos/`)).json();
     $currentVideo = videos.find((v) => v.id === urlParams.video);
 
     const poses = await (
       await fetch(`${API_BASE}/poses/${$currentVideo.id}/${urlParams.frame}/`)
     ).json();
-    $currentPose = poses[urlParams.pose];
+    $currentPose = poses[urlParams.pose - 1];
   };
 </script>
 
 {#await loadDataFromParams()}
   <p>loading data...</p>
 {:then}
-  <slot />
+  {#if $currentVideo && $currentPose}
+    <slot />
+  {:else}
+    <p>
+      Can't find pose #{urlParams.pose - 1} in frame #{urlParams.frame} of video
+      {urlParams.video}
+    </p>
+  {/if}
 {/await}
