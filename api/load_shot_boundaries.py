067a598eb819711cffea09d09d8ccecc7023d4f3
Peter Broadwell <broadwell@stanford.edu>
2024-01-08 23:33:53 -0800

    Incidental correction to the shot boundaries script

--- api/load_shot_boundaries.py
+++ api/load_shot_boundaries.py
@@ -8,9 +8,8 @@ import logging
 import pickle
 from pathlib import Path
 
-from rich.logging import RichHandler
-
 from mime_db import MimeDb
+from rich.logging import RichHandler
 
 SHOT_DETECT_THRESHOLD = 0.5
 BATCH_SIZE = 1000
