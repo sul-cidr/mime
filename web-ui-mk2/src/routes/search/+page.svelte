e82dc15641c66ac2d4d6978cb2c0ce8d9c6e00f5
Simon Wiles <simonjwiles@gmail.com>
2024-05-21 21:31:36 -0700

    Search page framework
    nb. this depends on an update I've made to the Carbon Tabs component to allow it to work with Svelte 5.

---
+++ web-ui-mk2/src/routes/search/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { Tabs, Tab, TabContent } from '/node_modules/carbon-components-svelte/src/index.js';
+
+	let selected = $state(0);
+</script>
+
+<section>
+	<div id="query-container">
+		<header>Source Pose</header>
+		<Tabs bind:selected>
+			<Tab label="Webcam" />
+			<Tab label="Pose Editor" />
+			<svelte:fragment slot="content">
+				<TabContent>Webcam Input goes here...</TabContent>
+				<TabContent>Pose Editor goes here...</TabContent>
+			</svelte:fragment>
+		</Tabs>
+	</div>
+	<div id="results-container"></div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		height: 100%;
+		width: 100%;
+
+		header {
+			display: flex;
+			padding: 1rem;
+			background-color: var(--panel-background);
+			border-bottom: 2px solid var(--primary);
+		}
+
+		div {
+			border: 1px solid var(--primary);
+		}
+
+		#query-container {
+			flex: 0 0 324px;
+			max-width: 324px;
+			overflow: hidden;
+		}
+
+		#results-container {
+			flex: 1 1 auto;
+		}
+	}
+</style>
