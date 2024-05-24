339ce18305f291d6b6ec342aeb9a9a894b71beda
Simon Wiles <simonjwiles@gmail.com>
2024-05-24 13:04:56 -0700

    A little more typing work

--- web-ui-mk2/src/lib/types.js
+++ web-ui-mk2/src/lib/types.js
@@ -1,4 +1,5 @@
 /** @typedef {Array<number>} Coco13SkeletonNoConfidence */
+/** @typedef {Array<number>} Coco13SkeletonWithConfidence */
 /** @typedef {Array<{x: number, y: number, z?: number}>} Coco13Pose */
 
 /**
@@ -6,6 +7,16 @@
  * @property {Coco13SkeletonNoConfidence} keypoints
  * @property {Array<number>} bbox [x0, y0, width, height]
  * @property {Coco13SkeletonNoConfidence} norm
+ */
+
+/**
+ * @typedef {Object} PoseDbFields
+ * @property {string} video_id
+ * @property {string} video_name
+ * @property {number} frame
+ * @property {number} pose_idx
+ * @property {number} [distance]
+ * @typedef {MinimalPose & PoseDbFields} PoseRecord
  */
 
 /**
