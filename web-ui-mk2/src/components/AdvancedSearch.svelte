7f99927a5755cf683dda4b56f2be9a21122cd33d
Peter Broadwell <broadwell@stanford.edu>
2025-08-09 00:13:50 -0700

    Refine UI/UX for pose prevalence chart displays

--- web-ui-mk2/src/components/AdvancedSearch.svelte
+++ web-ui-mk2/src/components/AdvancedSearch.svelte
@@ -1,6 +1,13 @@
 <script>
 	import { fade } from 'svelte/transition';
-	import { Loading, MultiSelect } from 'carbon-components-svelte';
+	import {
+		Button,
+		Loading,
+		MultiSelect,
+		Select,
+		SelectItem,
+		Toggle
+	} from 'carbon-components-svelte';
 	import { getVideoData } from '$lib/data-fetching';
 	import localStorageState from '$lib/localstorage.svelte';
 	import PosePrevalence from './PosePrevalence.svelte';
@@ -13,10 +20,14 @@
 	/** @type {SearchResultsProps} */
 	let { sourcePose } = $props();
 
-	/** @type {{value: 'cosine'|'euclidean'|'view_invariant'|'3d'}}} */
+	/** @type {{value: 'cosine'|'view_invariant'|'3d'}}} */
 	let searchType = localStorageState('pose-search-type', 'view_invariant');
 
 	let selectedVideoIds = $state([]);
+
+	// Some extra state to signal that running with "All" toggled on is a Big Deal
+	let allSelected = $state(false);
+	let runWithAll = $state(false);
 
 	let videoDataById = {};
 
@@ -35,38 +46,77 @@
 		searchType.value === '3d' && !sourcePose.fromWebcam
 			? sourcePose.global3d_coco13.toString()
 			: sourcePose.norm.toString();
+
+	const toggleAllVideos = (e) => {
+		const toggledOn = e.detail.toggled;
+		if (toggledOn) {
+			selectedVideoIds = Object.keys(videoDataById);
+			allSelected = true;
+			runWithAll = false;
+		} else {
+			selectedVideoIds = [];
+			allSelected = false;
+			runWithAll = false;
+		}
+	};
 </script>
 
 <div class="controls">
-	<label>
-		{#await formatVideoData()}
-			<div class="loading"><Loading small withOverlay={false} />Loading performances...</div>
-		{:then videos}
-			<div class="selector" transition:fade>
-				<MultiSelect
-					titleText="Performances"
-					label="Select performances..."
-					hideLabel
-					placeholder="Filter performances..."
-					filterable
-					items={videos}
-					bind:selectedIds={selectedVideoIds}
-				/>
+	{#await formatVideoData()}
+		<div class="loading"><Loading small withOverlay={false} />Loading performances...</div>
+	{:then videos}
+		<label class="selector-box">
+			<div>All</div>
+			<Toggle
+				labelA=""
+				labelB=""
+				hideLabel
+				labelText="All"
+				bind:allSelected
+				on:toggle={(e) => toggleAllVideos(e)}
+			/>
+		</label>
+		<label>
+			<div class="selector-box">
+				<span>Performances: </span>
+				<div class="selector" transition:fade>
+					<MultiSelect
+						titleText="Performances"
+						label="Select performances..."
+						disabled={allSelected}
+						hideLabel
+						placeholder="Type to filter..."
+						filterable
+						items={videos}
+						bind:selectedIds={selectedVideoIds}
+					/>
+				</div>
 			</div>
-		{/await}
-	</label>
+		</label>
+	{/await}
 	<label>
-		Search Type:
-		<select bind:value={searchType.value}>
-			<option value="cosine">Cosine</option>
-			<option value="euclidean">Euclidean</option>
-			<option value="view_invariant">View Invariant</option>
-			<option value="3d">Global 3D</option>
-		</select>
+		<Select
+			inline
+			labelText="Search Type:"
+			selected={searchType.value}
+			on:change={(e) => (searchType.value = e.target.value)}
+		>
+			<SelectItem value="cosine" text="Cosine" />
+			<SelectItem value="view_invariant" text="View Invariant" />
+			<SelectItem value="3d" text="Global 3D" />
+		</Select>
 	</label>
+	{#if sourcePose && allSelected && !runWithAll}
+		<Button
+			iconDescription="Run All"
+			kind="danger-tertiary"
+			size="sm"
+			on:click={() => (runWithAll = true)}>Run All (careful!)</Button
+		>
+	{/if}
 </div>
 
-{#if sourcePose}
+{#if sourcePose && (!allSelected || (allSelected && runWithAll))}
 	<div class="results">
 		{#each selectedVideoIds as videoId}
 			<PosePrevalence
@@ -86,14 +136,15 @@
 	.results {
 		display: flex;
 		flex-wrap: wrap;
-		gap: 1rem;
 		justify-content: center;
 		padding: 1rem;
+		gap: 1rem;
 	}
 
 	.loading {
 		display: flex;
 		flex-direction: column;
+		padding: 1rem;
 		gap: 1rem;
 		text-align: center;
 	}
@@ -102,11 +153,27 @@
 		width: 30rem;
 	}
 
+	.selector-box {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		font-size: 0.75rem;
+		color: #525252;
+		font-weight: 400;
+		letter-spacing: 0.32px;
+		line-height: 1rem;
+	}
+
+	.search-type {
+		width: 20rem;
+	}
+
 	label {
 		align-items: flex-start;
 		display: flex;
 		gap: 0.5rem;
 		line-height: 24px;
+		background-color: #f4f4f4;
 	}
 
 	input[type='number'] {
