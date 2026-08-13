1c1eba714ff603d7fc72ed79c46bf81914d071bd
Peter Broadwell <broadwell@stanford.edu>
2026-08-13 15:22:09 -0700

    Convert Vector type to list in API JSON responses
    This shouldn't be necessary -- the register_vector call in api/mime_db/__init__.py should handle it -- but that apparently stopped working with a recent update to the pgvector libraries, and this was the cleanest workaround.

--- api/lib/json_encoder.py
+++ api/lib/json_encoder.py
@@ -5,6 +5,7 @@ from uuid import UUID
 import asyncpg
 import numpy as np
 from asyncpg.pgproto import pgproto  # type: ignore
+from pgvector.vector import Vector
 
 
 class MimeJSONEncoder(json.JSONEncoder):
@@ -17,6 +18,7 @@ class MimeJSONEncoder(json.JSONEncoder):
             np.ndarray: lambda arr: arr.tolist(),
             pgproto.UUID: str,
             UUID: str,
+            Vector: lambda vec: vec.to_list(),
         }
         conversion_func = type_map.get(type(obj), super().default)
         return conversion_func(obj)
